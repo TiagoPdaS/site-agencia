976ce7209c49c50fbc79037a0b9c023002fae724
myuser <myemail>
2025-12-27 16:47:56 -0700

    start project
    dowload project by materialize

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav({edge: 'right'});
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
